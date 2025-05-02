5a1c77aa7b30c79da90b75e3fdc806465d0773ee
argha-saha <saha56@purdue.edu>
2025-05-02 15:17:04 -0400

    Updated cache config struct with policy types

--- include/CacheConfig.h
+++ include/CacheConfig.h
@@ -2,9 +2,30 @@
 
 #include <string>
 
+#include "Policies.h"
+
+/** 
+ * @brief Configuration struct holding all parameters to define
+ * the properties of a single cache level within the hierarchy.
+ */
 struct CacheConfig {
-    std::string name;
-    uint64_t capacity;      // In bytes
-    uint32_t block_size;    // In bytes
-    uint32_t associativity; // 1 for direct-mapped, >1 for n-way set associative
+    // Cache identification (e.g., L1, L2, L3)
+    std::string name = "";
+
+    // Cache size properties in bytes
+    uint64_t capacity = 0;
+    uint32_t block_size = 0;
+
+    /**
+     * @brief Number of blocks in each set.
+     * - Set to 1 for direct-mapped cache
+     * - Set to n for n-way set associative cache
+     * - Set to total number of blocks for fully associative cache
+     */
+    uint32_t associativity = 0;
+
+    /** Cache policies */
+    EvictionPolicyType eviction_policy = EvictionPolicyType::LRU;
+    WritePolicyType wrote_policy = WritePolicyType::WRITE_BACK;
+    AllocationPolicyType allocation_policy = AllocationPolicyType::WRITE_ALLOCATE;
 };
