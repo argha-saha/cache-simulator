c7cfdde3c090f636b2c86efc55cef5ad21a01a89
Argha Saha <arghasaha@icloud.com>
2025-05-03 15:24:59 -0400

    Wrote on_fill function for FIFO policy class

--- src/FIFOPolicy.cpp
+++ src/FIFOPolicy.cpp
@@ -7,7 +7,20 @@ void FIFOPolicy::initialize(uint32_t ways) {
 
 void FIFOPolicy::on_access(const CacheSet& set, uint32_t index) {}
 
-void FIFOPolicy::on_fill(const CacheSet& set, uint32_t index) {}
+void FIFOPolicy::on_fill(const CacheSet& set, uint32_t index) {
+    bool found = false;
+    
+    for (const auto& i : fifo) {
+        if (i == index) {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        fifo.push_back(index);
+    }
+}
 
 uint32_t FIFOPolicy::get_victim_index(const CacheSet& set) {}
 
