f5dbd87be0e666d1fe0043004cd15a07892f61df
argha-saha <saha56@purdue.edu>
2025-06-03 00:19:11 -0700

    Wrote header file for MemorySystem

---
+++ include/MemorySystem.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "CacheHierarchy.h"
+#include <vector>
+
+class MemorySystem {
+public:
+    MemorySystem() = default;
+    virtual ~MemorySystem() = default;
+
+    // Prevent copying and moving
+    MemorySystem(const MemorySystem&) = delete;
+    MemorySystem& operator=(const MemorySystem&) = delete;
+    MemorySystem(MemorySystem&&) = delete;
+    MemorySystem& operator=(MemorySystem&&) = delete;
+
+    void configure_cache(const std::vector<CacheConfig>& configurations);
+
+    void execute_access(uint64_t address, char access_type);
+
+    void print_statistics() const;
+
+private:
+    CacheHierarchy hierarchy;
+};
