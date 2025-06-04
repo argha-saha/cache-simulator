c5a199f6b4ccaa0aa5ea802b81453a75b0ad77a9
argha-saha <saha56@purdue.edu>
2025-06-03 19:04:31 -0700

    Fixed issue with incorrect exception for cache capacity

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+#include "CacheConfig.h"
+#include "Policies.h"
+#include "CacheLevel.h"
+#include <iostream>
+
 int main() {
+    CacheConfig config;
+    config.name = "L1";
+    config.capacity = 1024; // 1KB
+    config.block_size = 64; // 64B
+    config.associativity = 4; // 4-way set associative
+    config.eviction_policy = EvictionPolicyType::LRU;
+    config.write_policy = WritePolicyType::WRITE_BACK;
+    config.allocation_policy = AllocationPolicyType::WRITE_ALLOCATE;
+
+    CacheLevel cache_level(config, nullptr);
+    
     return 0;
 }
